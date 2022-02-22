37d97208febcf61a132ca6f87b7fb6f5b165cf54
csqrl <21029087+csqrl@users.noreply.github.com>
2022-02-22 18:18:14 +0000

    Implement collapsible sections

--- src/Components/Layout/Forms/Section.lua
+++ src/Components/Layout/Forms/Section.lua
@@ -1,13 +1,14 @@
-local Packages = script.Parent.Parent.Parent.Parent.Packages
+local Plugin = script.Parent.Parent.Parent.Parent
 
-local Roact = require(Packages.Roact)
-local Hooks = require(Packages.Hooks)
-local StudioTheme = require(Packages.StudioTheme)
+local Roact = require(Plugin.Packages.Roact)
+local Hooks = require(Plugin.Packages.Hooks)
+local StudioTheme = require(Plugin.Packages.StudioTheme)
 
-local ListLayout = require(script.Parent.Parent.ListLayout)
-local Text = require(script.Parent.Parent.Parent.Text)
-local Divider = require(script.Parent.Parent.Divider)
-local Frame = require(script.Parent.Parent.Frame)
+local ListLayout = require(Plugin.Components.Layout.ListLayout)
+local Divider = require(Plugin.Components.Layout.Divider)
+local Frame = require(Plugin.Components.Layout.Frame)
+local Text = require(Plugin.Components.Text)
+local Icon = require(Plugin.Components.Icon)
 
 local e = Roact.createElement
 
@@ -17,10 +18,17 @@ export type FormSectionProps = {
 	heading: string?,
 	hint: string?,
 	formItem: boolean?,
+	collapsed: boolean?,
+	collapsible: boolean?,
 }
 
 local function FormSection(props: FormSectionProps, hooks)
+	local collapsed, setCollapsed = hooks.useState(props.collapsed)
 	local theme, styles = StudioTheme.useTheme(hooks)
+
+	local function onActivated()
+		setCollapsed(not collapsed)
+	end
 
 	return e(Frame, {
 		order = props.order,
@@ -30,31 +38,53 @@ local function FormSection(props: FormSectionProps, hooks)
 			gap = props.formItem and styles.spacing * 0.5,
 		}),
 
-		divider = if props.divider
-			then e(Divider, {
-				order = 0,
-			})
-			else nil,
+		divider = props.divider and e(Divider, {
+			order = 0,
+		}),
 
-		heading = if props.heading
-			then e(Text, {
+		heading = props.heading and e("ImageButton", {
+			AutomaticSize = Enum.AutomaticSize.Y,
+			BackgroundTransparency = 1,
+			LayoutOrder = 10,
+			Size = UDim2.fromScale(1, 0),
+
+			[Roact.Event.Activated] = props.collapsible and onActivated,
+		}, {
+			layout = e(ListLayout, {
+				direction = Enum.FillDirection.Horizontal,
+			}),
+
+			icon = props.collapsible and e("Frame", {
+				AutomaticSize = Enum.AutomaticSize.XY,
+				BackgroundTransparency = 1,
+				LayoutOrder = 10,
+				Size = UDim2.new(),
+			}, {
+				image = e(Icon, {
+					icon = "Caret",
+					colour = theme:GetColor(Enum.StudioStyleGuideColor.BrightText),
+					rotation = collapsed and -90 or 0,
+					order = 10,
+				}),
+			}),
+
+			label = e(Text, {
 				text = props.heading,
 				textColour = theme:GetColor(Enum.StudioStyleGuideColor.BrightText),
+				size = props.collapsible and UDim2.new(1, -16, 0, 0),
 				font = not props.formItem and styles.font.semibold,
-				order = 10,
-			})
-			else nil,
+				order = 20,
+			}),
+		}),
 
-		hint = if props.hint
-			then e(Text, {
+		collapsible = not collapsed and Roact.createFragment({
+			hint = props.hint and e(Text, {
 				text = props.hint,
 				textColour = theme:GetColor(Enum.StudioStyleGuideColor.DimmedText),
 				order = 20,
-			})
-			else nil,
+			}),
 
-		content = if props[Roact.Children]
-			then e(Frame, {
+			content = props[Roact.Children] and e(Frame, {
 				order = 30,
 			}, {
 				layout = e(ListLayout, {
@@ -62,8 +92,8 @@ local function FormSection(props: FormSectionProps, hooks)
 				}),
 
 				Roact.createFragment(props[Roact.Children]),
-			})
-			else nil,
+			}),
+		}),
 	})
 end
 
